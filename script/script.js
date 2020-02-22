d50c1e4d69d8bc5f775584bb565e0c609bd17aed
Dmitry Mikhaylov <dmikhaylov7@gmail.com>
2020-02-22 17:38:58 +0300

    Задание 20

--- script/script.js
+++ script/script.js
@@ -59,27 +59,27 @@ document.addEventListener('DOMContentLoaded', () => {
     //Меню
     const toggleMenu = () => {
         const btnMenu = document.querySelector('.menu'),
-            menu = document.querySelector('menu'),
-            menuItems = menu.querySelectorAll('ul>li');
+            menu = document.querySelector('menu');
+
 
         btnMenu.addEventListener('click', () => {
             menu.classList.toggle('active-menu');
         });
         menu.addEventListener('click', (event) => {
-            let target = event.target;
-            console.log(target);
+            const target = event.target;
+
             if (target.classList.contains('close-btn')) {
                 menu.classList.toggle('active-menu');
             }
-            if ( target.closest('li')) {
+            if (target.closest('li')) {
                 menu.classList.toggle('active-menu');
             }
-            
+
         });
     };
     toggleMenu();
 
-    //popup
+    //Pop-Up
     const togglePopUp = () => {
         const popUp = document.querySelector('.popup'),
             popupBtn = document.querySelectorAll('.popup-btn'),
@@ -137,8 +137,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     togglePopUp();
 
-    //табы
-
+    //Tабы
     const tabs = () => {
         const tabHeader = document.querySelector('.service-header'),
             tab = tabHeader.querySelectorAll('.service-header-tab'),
@@ -172,4 +171,120 @@ document.addEventListener('DOMContentLoaded', () => {
 
     };
     tabs();
+
+    //Слайдер
+    const slider = () => {
+        const slide = document.querySelectorAll('.portfolio-item'),
+            btn = document.querySelectorAll('.portfolio-btn'),
+            slider = document.querySelector('.portfolio-content'),
+            dotsUl = document.querySelector('.portfolio-dots');
+            
+
+        //Переменная, которая будет содежать индекс текущего слайда, интервал и точку
+        let currentSlide = 0,
+            interval,
+            dots = null;
+
+        //Проверяем кол-во слайдеров и создаем необходимое кол-во точек
+        const createPoints = () => {
+            for ( let i = 0; i < slide.length; i++ ) {
+                    let li = document.createElement('li');
+                    li.classList.add('dot');
+                    dotsUl.appendChild(li);
+            }
+            dots = dotsUl.querySelectorAll('.dot');
+        };
+
+        //Функция смены слайда
+        const prevSlide = (elem, index, strClass) => {
+            elem[index].classList.remove(strClass);
+
+        };
+        const nextSlide = (elem, index, strClass) => {
+            elem[index].classList.add(strClass);
+        };
+
+        //автоматическое прелистывание слайдера
+        const autoPlaySlide = () => {
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dots, currentSlide, 'dot-active');
+            currentSlide++;
+            //Ограничиваем кол-во слайдов
+            if(currentSlide >= slide.length) {
+                currentSlide = 0;
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active' );
+            nextSlide(dots, currentSlide, 'dot-active');
+
+        };
+
+        //Запуск и остановка слайдера
+        const startSlider = (time = 2500) => {
+            interval = setInterval(autoPlaySlide, time);
+        };
+        const stopSlider = () => {
+            clearInterval(interval);
+
+        };
+        //Взаимодействие со слайдером вручную
+        slider.addEventListener('click', (event) => {
+            event.preventDefault();
+            
+            let target = event.target;
+
+            //Ограничения на клик(если кликнули не на один из элементов)
+            if(!target.matches('.portfolio-btn, .dot')) {
+                return;
+            }
+
+            //Убираем активные классы утекущего слайда
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dots, currentSlide, 'dot-active');
+
+            //Проверяем на какую кнопку нажали
+            if(target.matches('#arrow-right')) {
+                currentSlide++;
+            }else if(target.matches('#arrow-left')) {
+                currentSlide--;
+
+            //Присваиваем индекс исходя из того на какую точку мы кликнули
+            } else if (target.matches('.dot')) {
+                dots.forEach((elem, index) => {
+                    if (elem === target) {
+                        currentSlide = index;
+                    }
+                });
+            }
+
+            if(currentSlide >= slide.length) {
+                currentSlide = 0;
+            }
+
+            if(currentSlide < 0) {
+                currentSlide = slide.length - 1;
+            }
+            //Добавляем классы тому слайду, который получился после проверки на условия
+            nextSlide(slide, currentSlide, 'portfolio-item-active' );
+            nextSlide(dots, currentSlide, 'dot-active');
+        });
+
+        slider.addEventListener('mouseover', (event) => {
+            if(event.target.matches('.portfolio-btn') || 
+            event.target.matches('.dot')) {
+                stopSlider();
+            }
+        });
+        slider.addEventListener('mouseout', (event) => {
+            if(event.target.matches('.portfolio-btn') || 
+            event.target.matches('.dot')) {
+                startSlider();
+            }
+        });
+        createPoints();
+        startSlider(2500);
+
+        
+    };
+
+    slider();
 });
